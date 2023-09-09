4c9b5884335a13e8119c3eb388495ea1ee6322aa
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-09-09 11:46:04 -0600

    some instruction added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,14 @@
 import './County.css'
 const Country = ({country}) => {
     console.log(country)
-    const {name, flags} =country;
+    const {name, flags, population, area
+    } =country;
     return (
         <div className='box'>
             <h3>Name: {name.common}</h3>
             <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area:{area} </p>
         </div>
     );
 };
